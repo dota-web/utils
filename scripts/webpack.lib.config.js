24856289a48657127aecdcbea4c57c81683e5695
hexing <hexing@zhongan..com>
2021-06-03 18:59:47 +0800

    增加webpack lib config

---
+++ scripts/webpack.lib.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: './utils/index.ts'
+  },
+  output: {
+    filename: '[name].lib.js',
+    clean: true,
+    path: path.resolve(__dirname, '../lib'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts)?$/,
+        use: [
+          {
+            loader: 'babel-loader?cacheDirectory=true',
+            options: {
+              presets: ['@babel/preset-env']
+            },
+          },
+          'ts-loader' 
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+}
